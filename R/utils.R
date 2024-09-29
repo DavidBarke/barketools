9a53c4967fb3e58c887802ca24bd6f4281bead41
David Barkemeyer <david.barkemeyer@gmx.de>
2024-09-29 15:42:57 -0400

    Add apply_if, improve documentation

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,13 @@
+#' Split a vector into modulus batches
+#'
+#' @param x A vector.
+#' @param n An integer.
+#'
+#' @returns A list with at most `n` elements. The `i`-th element is a vector
+#' containing all the elements of `x` belonging to the same equivalence class
+#' with respect to the `modulus` operation. That means \eqn{x_1} and \eqn{x_2}
+#' are both elements of `out[i]` if eqn{x_1 mod n = x_2 mod n = i}.
+#'
 #' @export
 index_split <- function(x, n) {
   if (length(x) <= n) return(purrr::map(x, identity))
@@ -8,6 +18,14 @@ index_split <- function(x, n) {
 
 
 
+#' Split a vector into sequential batches
+#'
+#' @param x A vector.
+#' @param batch_size An integer.
+#'
+#' @returns A list where each element contains `batch_size` elements of `x`. The
+#' last element of the list contains at most `batch_size` elements of `x`.
+#'
 #' @export
 batch_vector <- function(x, batch_size) {
   n_x <- length(x)
@@ -21,6 +39,10 @@ batch_vector <- function(x, batch_size) {
 
 
 
+#' Print number of rows
+#'
+#' @param df Data frame.
+#'
 #' @export
 print_nrow <- function(df) {
   n <- nrow(df)
@@ -30,8 +52,28 @@ print_nrow <- function(df) {
 
 
 
+#' Print object if condition holds
+#'
+#' @param x Object to be printed conditionally.
+#' @param cond Logical. If `TRUE`, `x` is printed.
+#'
 #' @export
 print_if <- function(x, cond) {
   if (cond) print(x)
   x
+}
+
+
+
+#' Apply function if condition holds
+#'
+#' @param x Object to which function is applied conditionally.
+#' @param cond Logical.
+#' @param f Function that takes one argument `x`.
+#'
+#' @returns If `cond` `f(x)`, else `x`.
+#'
+#' @export
+apply_if <- function(x, cond, f) {
+  if (cond) f(x) else x
 }
