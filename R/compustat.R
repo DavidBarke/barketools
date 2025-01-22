8ca41b31e7a54fefc357cc1879ae02bd263bc159
David Barkemeyer <david.barkemeyer@gmx.de>
2025-01-22 14:42:07 -0500

    Remove NAs

--- R/compustat.R
+++ R/compustat.R
@@ -1,6 +1,6 @@
 #' @export
 pad_gvkey <- function(gvkey) {
-  if (!all(stringr::str_detect(gvkey, "^\\d{4,6}$"))) {
+  if (!all(stringr::str_detect(gvkey, "^\\d{4,6}$"), na.rm = TRUE)) {
     cli::cli_abort("'gvkey' has to consist of 4 to 6 digits.")
   }
   stringr::str_pad(gvkey, 6, "left", "0")
