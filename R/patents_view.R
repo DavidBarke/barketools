95380708983c4279d844f18ccdf17d6d514ec727
David Barkemeyer <david.barkemeyer@gmx.de>
2025-05-28 20:46:21 -0400

    Update patents_view_assignee_type_tbl

--- R/patents_view.R
+++ R/patents_view.R
@@ -16,31 +16,34 @@ patents_view_assignee_type_map <- function() {
 #'
 #' @returns A tibble with the following columns:
 #' * `assignee_type` (numeric). PatentsView assignee type.
+#' * `assignee_type_2` (character). Assignee category, e.g., `"Firm"` or
+#' `"Government"`.
 #' * `assignee_type_str` (character). Textual description of assignee type.
+#' * `assignee_region` (character). `"US"` or `"Foreign"`.
 #' * `partial_interest` (logical). `TRUE` if partial interest.
 #'
 #' @export
 patents_view_assignee_type_tbl <- function() {
   tbl <- tibble::tribble(
-    ~assignee_type, ~assignee_type_str,
-    0, "0",
-    1, "Unassigned",
-    2, "US Company or Corporation",
-    3, "Foreign Company or Corporation",
-    4, "US Individual",
-    5, "Foreign Individual",
-    6, "US Federal Government",
-    7, "Foreign Government",
-    8, "US County Government",
-    9, "US State Government",
-    12, "US Company or Corporation",
-    13, "Foreign Company or Corporation",
-    14, "US Individual",
-    15, "Foreign Individual",
-    16, "US Federal Government",
-    17, "Foreign Government",
-    18, "US County Government",
-    19, "US State Government"
+    ~assignee_type, ~assignee_type_2, ~assignee_type_str, ~assignee_region,
+    0, "0", "0", NA,
+    1, "Unassigned", "Unassigned", NA,
+    2, "Firm", "US Company or Corporation", "US",
+    3, "Firm", "Foreign Company or Corporation", "Foreign",
+    4, "Individual", "US Individual", "US",
+    5, "Individual", "Foreign Individual", "Foreign",
+    6, "Government", "US Federal Government", "US",
+    7, "Government", "Foreign Government", "Foreign",
+    8, "Government", "US County Government", "US",
+    9, "Government", "US State Government", "US",
+    12, "Firm", "US Company or Corporation", "US",
+    13, "Firm", "Foreign Company or Corporation", "Foreign",
+    14, "Individual", "US Individual", "US",
+    15, "Individual", "Foreign Individual", "Foreign",
+    16, "Government", "US Federal Government", "US",
+    17, "Government", "Foreign Government", "Foreign",
+    18, "Government", "US County Government", "US",
+    19, "Government", "US State Government", "US"
   )
   tbl$partial_interest <- tbl$assignee_type %in% 12:19
   tbl
