732c2ac0feb5ba518e8a3d921198b979fd405aec
David Barkemeyer <david.barkemeyer@gmx.de>
2025-07-29 00:46:30 -0400

    Add winsorize and winsorize_bounds

--- R/math.R
+++ R/math.R
@@ -4,11 +4,15 @@ max_or_na <- function(x) {
   max(x, na.rm = TRUE)
 }
 
+
+
 #' @export
 min_or_na <- function(x) {
   if (all(is.na(x))) return(NA)
   min(x, na.rm = TRUE)
 }
+
+
 
 #' @export
 psum <- function(..., na.rm = TRUE) {
@@ -17,6 +21,8 @@ psum <- function(..., na.rm = TRUE) {
   })
 }
 
+
+
 #' @export
 pprod <- function(..., na.rm = TRUE) {
   purrr::pmap(list(...), \(...) {
@@ -24,9 +30,56 @@ pprod <- function(..., na.rm = TRUE) {
   })
 }
 
+
+
 #' @export
 pall <- function(..., na.rm = TRUE) {
   purrr::pmap(list(...), \(...) {
     all(..., na.rm = na.rm)
   })
+}
+
+
+
+#' Winsorize
+#'
+#' Winsorize a numerical vector.
+#'
+#' @inheritParams winsorize_bounds
+#'
+#' @export
+winsorize <- function(x, probs) {
+  bounds <- winsorize_bounds(x, probs = probs)
+  dplyr::if_else(
+    x < bounds[1],
+    bounds[1],
+    dplyr::if_else(
+      x > bounds[2],
+      bounds[2],
+      x
+    )
+  )
+}
+
+
+
+#' Winsorize Bounds
+#'
+#' Get winsorize bounds for a numerical vector. This function is effectively
+#' a wrapper around [stats:quantile].
+#'
+#' @param x Numerical vector.
+#' @param probs A numerical vector of length 2 specifying the quantiles at which
+#' to winsorize. Use `0` or `1` if you do not want to winsorize at the lower end
+#' or the upper end, respectively.
+#'
+#' @returns A numerical vector of length 2. The first element is the lower bound
+#' and the second element is the upper bound.
+#'
+#' @export
+winsorize_bounds <- function(x, probs) {
+  stopifnot(length(probs) == 2)
+  quantile(x, probs = probs, type = 1, na.rm = TRUE) |>
+    sort() |>
+    unname()
 }
