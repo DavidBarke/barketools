8d85574bc226b1df661f238e643df74483721832
David Barkemeyer <david.barkemeyer@gmx.de>
2025-07-31 05:20:26 -0400

    Add arrow_latest_release

--- R/arrow.R
+++ R/arrow.R
@@ -14,3 +14,32 @@ arrow_dataset_files <- function(sources) {
     }) |>
     purrr::list_rbind()
 }
+
+
+
+#' Filter Dataset for Latest Release
+#'
+#' This function determines the latest release of a dataset that has been
+#' partitioned by date and applies a filter for the latest release.
+#'
+#' @param ds A `Dataset` as returned by [arrow:open_dataset].
+#' @param release_column The name of a column which can be interpreted as
+#' a release date. Optimally, the dataset has been partitioned along this
+#' column so that only the latest release data has to be collected in a
+#' subsequent step.
+#'
+#' @returns A `Dataset` with filter for `<release_column> == <latest_release>`.
+#'
+#' @export
+arrow_latest_release <- function(ds, release_column = "release") {
+  releases <- ds |>
+    dplyr::distinct(.data[[release_column]]) |>
+    dplyr::collect() |>
+    dplyr::pull(.data[[release_column]]) |>
+    lubridate::as_date()
+
+  latest_release <- max(releases)
+
+  ds |>
+    dplyr::filter(.data[[release_column]] == .env$latest_release)
+}
